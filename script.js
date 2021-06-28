77ab1f94bc882e0c0bf385cc72c832207a406eb5
Rizwan Shaikh <shaikhrizwanali123@gmail.com>
2021-06-28 20:47:09 +0530

    made some minor changes

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: [ "Web Developer", "Blogger", "Designer"],
+        strings: [ "Web Developer", "Blogger", "Programmer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
